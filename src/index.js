548442dc0b2d4f01a969d6ed92e1a937ea50615b
rabie <r.raad@xpay.de>
2021-04-21 11:17:10 +0200

    ready to be shipped

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import * as styles from "./shared/styles"
+import * as global from "./shared/global"
+import * as animation from "./shared/animation"
+import * as icons from "./shared/icons"
+
+export { styles, global, animation, icons }
+
+export * from "./components/Avatar"
+export * from "./components/Badge"
+export * from "./components/Button"
+export * from "./components/Icon"
+export * from "./components/Link"
